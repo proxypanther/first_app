da7a2cca80af18cbdbad9adf197765c3480006c0
ProxyPanther <proxypanther@gmail.com>
2014-03-13 10:33:29 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e6abe05ef01eaf90e5ac86a0ed2c4e5f873eaa05681427fbc24104616b9476f069a420790c52b5a16487ffbc64a7e5018902202fb16ced5b0ba06eaddb2b6b7c'
